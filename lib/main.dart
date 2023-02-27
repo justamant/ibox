33e934aaceea863c98e1dab28eaba745904a9e39
kimberly <kimberlyjustamant7@gmail.com>
2023-02-27 09:58:38 -0300

    oi

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+ 
+import 'package:flutter/material.dart
+
+void main(List<String> args) {} {}
+  
+
+
+
+
+class Myapp extends StatelessWidget {
+  @override 
+  Widget build(context){
+    return MaterialApp()
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text
+        ( "meu primeiro  app ",
+        style: TextStyle,)(Color: Colors.red),
+
+
+      ),
+    )
+  }
+}
